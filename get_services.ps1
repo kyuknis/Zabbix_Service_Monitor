b7a4b88dea53ff132925d7fa4e9293fe50e67b52
Kirkland <kirkland@yuknis.com>
2017-11-20 16:34:53 -0500

    Initial Commit

---
+++ get_services.ps1
@@ -0,0 +1,29 @@
+﻿################################################################################
+# get_services.ps1
+#
+################################################################################
+
+$services = Get-Service | select name
+$idx = 1
+
+Write-Host "{"
+Write-Host " `"data`":[`n"
+ForEach($s in $services) {
+
+    if ($idx -lt $services.count)
+    {
+        $line= "{ `"{#SERVICE}`" : `"" + $s.Name + "`" },"
+        write-host $line
+    }
+    elseif ($idx -ge $services.count)
+    {
+        $line= "{ `"{#SERVICE}`" : `"" + $s.Name + "`" }"
+        write-host $line
+    }
+
+    $idx++;
+
+}
+Write-Host
+Write-Host " ]"
+Write-Host "}"
