be97a370814104e7e1fe69eca6a38ce55f92fe62
Kirkland <kirkland@yuknis.com>
2018-05-30 11:52:29 -0400

    Added comments to better explain code

--- get_services.ps1
+++ get_services.ps1
@@ -3,11 +3,19 @@
 #
 ################################################################################
 
+#Get all of the Windows services
 $services = Get-Service | Select-Object name
+
+# Create the response template to build off of.
 $response = @{data = @()}
 
+# Add each of the services with the appropriate key ("{#SERVICE}") to the data array
 ForEach($s in $services) {
     $response['data'] += @{ '{#SERVICE}' = $s.name}
 }
 
-ConvertTo-Json $response
+# Convert  the response to JSON for Zabbix
+$response = ConvertTo-Json $response
+
+# Write the JSON encoded response for the Zabbix agent to send to the server
+Write-Host $response
