575db333573b3b8e7ab9b90dc4e3883bebb941f8
Kirkland <kirkland@yuknis.com>
2018-05-30 11:49:54 -0400

    Rewritten to make best use of standard Powershell cmdlets

--- get_services.ps1
+++ get_services.ps1
@@ -3,27 +3,11 @@
 #
 ################################################################################
 
-$services = Get-Service | select name
-$idx = 1
+$services = Get-Service | Select-Object name
+$response = @{data = @()}
 
-Write-Host "{"
-Write-Host " `"data`":[`n"
 ForEach($s in $services) {
-
-    if ($idx -lt $services.count)
-    {
-        $line= "{ `"{#SERVICE}`" : `"" + $s.Name + "`" },"
-        write-host $line
-    }
-    elseif ($idx -ge $services.count)
-    {
-        $line= "{ `"{#SERVICE}`" : `"" + $s.Name + "`" }"
-        write-host $line
-    }
-
-    $idx++;
-
+    $response['data'] += @{ '{#SERVICE}' = $s.name}
 }
-Write-Host
-Write-Host " ]"
-Write-Host "}"
+
+ConvertTo-Json $response
